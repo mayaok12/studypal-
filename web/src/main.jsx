193d0610f2265699feca18d009ca4a8f287bd54f
mayaok12 <maya@okada.uk>
2026-07-16 17:31:36 +0100

    main jsk code

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,10 +1,30 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
+import Dashboard from './pages/Dashboard'
+import Focus from './pages/Focus'
+import Tasks from './pages/Tasks'
+import Planner from './pages/Planner'
+import Stats from './pages/Stats'
+import Gems from './pages/Gems'
+import Friends from './pages/Friends'
 import './index.css'
-import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/focus" element={<Focus />} />
+          <Route path="/tasks" element={<Tasks />} />
+          <Route path="/planner" element={<Planner />} />
+          <Route path="/stats" element={<Stats />} />
+          <Route path="/gems" element={<Gems />} />
+          <Route path="/friends" element={<Friends />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
